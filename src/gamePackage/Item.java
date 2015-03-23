dd193191ac15c40ce0f71050b2c4096c5985b443
Michael Vagn Hansen <micvaha@hotmail.com>
2015-03-23 17:22:45 +0100

    item and weapons

---
+++ src/gamePackage/Item.java
@@ -0,0 +1,15 @@
+package gamePackage;
+
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Sound;
+
+public class Item extends GameObject {
+	public Image sprite_thumbnail;
+	public int attribute_value;
+	public String attribute_description;
+	public Sound sound_drop;
+
+	public void pickUp(){
+		
+	}
+}
---
+++ src/gamePackage/Weapon.java
@@ -0,0 +1,11 @@
+package gamePackage;
+
+public class Weapon {
+	
+	public int attribute_attackdmg;
+	public float attribute_attackspeed;
+	public int attribute_durability_max;
+	public int attribute_durability_current;
+	public boolean attribute_isRanged;
+
+}
