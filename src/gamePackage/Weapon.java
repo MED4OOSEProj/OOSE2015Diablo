364ec6d26c0b8be358289c9d9dc097f536813fe1
Michael Vagn Hansen <micvaha@hotmail.com>
2015-03-23 17:41:34 +0100

    WeaponCon

--- src/gamePackage/Weapon.java
+++ src/gamePackage/Weapon.java
@@ -7,5 +7,9 @@ public class Weapon extends Item {
 	public int attribute_durability_max;
 	public int attribute_durability_current;
 	public boolean attribute_isRanged;
+	
+	public Weapon (){
+	
+	}
 
 }
