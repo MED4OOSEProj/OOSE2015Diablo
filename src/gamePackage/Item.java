27acbc4f93fd3c628edce3dea188f304f093c9c7
yohark <yohasmail@mail.com>
2015-04-27 16:35:31 +0200

    Hubba bubba

--- src/gamePackage/Item.java
+++ src/gamePackage/Item.java
@@ -1,5 +1,7 @@
 package gamePackage;
 
+import java.util.Random;
+
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Sound;
 
@@ -10,5 +12,18 @@ public class Item extends GameObject {
 	public String attribute_description;
 	public Sound sound_drop;
 
-	
+	public void generateItem(int posX, int posY){
+		Random rnd = new Random();
+		int n = rnd.nextInt(2);
+		if (n == 0){
+			// create armor
+				//Armor.generateArmor();
+		} else if (n == 1){
+			// create potion
+				//Potion.generatePotion();
+		} else if (n == 2){
+			// create weapon
+			Weapon.generateWeapon();
+		}
+	}
 }
--- src/gamePackage/Weapon.java
+++ src/gamePackage/Weapon.java
@@ -1,5 +1,7 @@
 package gamePackage;
 
+import java.util.Random;
+
 public class Weapon extends Item {
 	
 	public int attribute_attackdmg;
@@ -8,8 +10,30 @@ public class Weapon extends Item {
 	public int attribute_durability_current;
 	public boolean attribute_isRanged;
 	
+	public String[] prefix = new String[]{"Brunhilde's ","The Powerful ","Unyielding ","The Monsterbane ","Death's ","Skullsplitter ","Spinecracking ","The Seagold ","Cursed ","Holy "}; 
+	
+	// weapontype 0-4 are melee, 5-9 are ranged
+	public String[] weaponType = new String[]{"Axe","Sword","Mace","Pike","Spear","Javelin","Crossbow","Bow","Longbow","Throwing Knife"};
+	
+	// 0-2 are high dmg, 3-5 are fast weapons, 6-8 are durable, 10 is the most powerful EVeR! 
+	public String[] suffix = new String[]{" of Mightiness"," of Slaying"," of the Knights Templar"," of Swiftslaying"," of Quicksilver"," of Quickness"," of Unreliable Power"," of the Unbreakable Vow"," of diamond endurance"," of Godslaying"};
+	
+	
 	public Weapon (){
 	
 	}
-
+	
+	
+	public static void generateWeapon(){
+		Weapon weapon = new Weapon();
+		Random rnd = new Random();
+		
+		weapon.attribute_description = "";
+		weapon.attribute_attackdmg = rnd.nextInt(10); 
+		weapon.attribute_attackspeed = rnd.nextFloat()*0.5f; 
+		weapon.attribute_durability_current = weapon.attribute_durability_max; 
+		weapon.attribute_durability_max = rnd.nextInt(10)*2; 
+		weapon.attribute_isRanged = rnd.nextBoolean(); 
+		
+	}
 }
