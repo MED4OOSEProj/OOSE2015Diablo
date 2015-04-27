1db15e732ddca4cd830612eb33aee0c55799dfac
yohark <yohasmail@gmail.com>
2015-04-27 11:31:48 +0200

    temporary map

--- src/gamePackage/GameLevel.java
+++ src/gamePackage/GameLevel.java
@@ -10,7 +10,19 @@ public class GameLevel {
 	}
 	
 	public void createRandomMap(){
-		
+		//temporary map generation
+		grid_terrainIDs = new int[][]{
+		        {1,1,1,1,1,1,1,1,1,1},
+		        {1,0,0,0,0,0,1,1,1,1},
+		        {1,0,1,1,1,0,1,1,1,1},
+		        {1,0,1,1,1,0,0,0,1,1},
+		        {1,0,0,0,1,1,1,0,1,1},
+		        {1,1,1,0,1,1,1,0,0,0},
+		        {1,0,1,0,0,0,0,0,1,0},
+		        {1,0,1,1,1,1,1,1,1,0},
+		        {1,0,0,0,0,0,0,0,0,0},
+		        {1,1,1,1,1,1,1,1,1,0}
+		    };
 	}
 	
 	public ArrayList<GameObject>[][] grid_objects;
