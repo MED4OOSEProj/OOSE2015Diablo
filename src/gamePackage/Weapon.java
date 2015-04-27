54d03092bd2e17c5e6d025ac013a3615f2970a7e
yohark <yohasmail@mail.com>
2015-04-27 17:01:35 +0200

    Weapon + items

--- src/gamePackage/Weapon.java
+++ src/gamePackage/Weapon.java
@@ -10,13 +10,13 @@ public class Weapon extends Item {
 	public int attribute_durability_current;
 	public boolean attribute_isRanged;
 	
-	public String[] prefix = new String[]{"Brunhilde's ","The Powerful ","Unyielding ","The Monsterbane ","Death's ","Skullsplitter ","Spinecracking ","The Seagold ","Cursed ","Holy "}; 
+	public static String[] prefix = new String[]{"Brunhilde's ","The Powerful ","Unyielding ","The Monsterbane ","Death's ","Skullsplitter ","Spinecracking ","The Seagold ","Cursed ","Holy "}; 
 	
 	// weapontype 0-4 are melee, 5-9 are ranged
-	public String[] weaponType = new String[]{"Axe","Sword","Mace","Pike","Spear","Javelin","Crossbow","Bow","Longbow","Throwing Knife"};
+	public static String[] weaponType = new String[]{"Axe","Sword","Mace","Pike","Spear","Javelin","Crossbow","Bow","Longbow","Throwing Knife"};
 	
 	// 0-2 are high dmg, 3-5 are fast weapons, 6-8 are durable, 10 is the most powerful EVeR! 
-	public String[] suffix = new String[]{" of Mightiness"," of Slaying"," of the Knights Templar"," of Swiftslaying"," of Quicksilver"," of Quickness"," of Unreliable Power"," of the Unbreakable Vow"," of diamond endurance"," of Godslaying"};
+	public static String[] suffix = new String[]{" of Mightiness"," of Slaying"," of the Knights Templar"," of Swiftslaying"," of Quicksilver"," of Quickness"," of Unreliable Power"," of the Unbreakable Vow"," of Diamond Endurance"," of Godslaying"};
 	
 	
 	public Weapon (){
@@ -28,12 +28,11 @@ public class Weapon extends Item {
 		Weapon weapon = new Weapon();
 		Random rnd = new Random();
 		
-		weapon.attribute_description = "";
 		weapon.attribute_attackdmg = rnd.nextInt(10); 
 		weapon.attribute_attackspeed = rnd.nextFloat()*0.5f; 
 		weapon.attribute_durability_current = weapon.attribute_durability_max; 
 		weapon.attribute_durability_max = rnd.nextInt(10)*2; 
 		weapon.attribute_isRanged = rnd.nextBoolean(); 
-		
+		weapon.attribute_description = prefix[rnd.nextInt(10)] + weaponType[rnd.nextInt(10)] + suffix[rnd.nextInt(10)];
 	}
 }
