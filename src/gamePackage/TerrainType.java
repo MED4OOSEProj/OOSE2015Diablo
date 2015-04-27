e83b347b13bf8c834aef62d038155d590909e8fe
yohark <yohasmail@gmail.com>
2015-04-27 12:33:36 +0200

    blocking

--- src/gamePackage/TerrainType.java
+++ src/gamePackage/TerrainType.java
@@ -7,11 +7,13 @@ public class TerrainType {
 	String description;
 	int movementcost;
 	Image terrainImage;
+	boolean blocksPath;
 	
-	public TerrainType(String name, int movementcost, String description, Image terrainImage){
+	public TerrainType(String name, int movementcost, String description, Image terrainImage, boolean blocksPath){
 		this.name = name;
 		this.description = description;
 		this.movementcost = movementcost;
 		this.terrainImage = terrainImage;
+		this.blocksPath = blocksPath;
 	}
 }
