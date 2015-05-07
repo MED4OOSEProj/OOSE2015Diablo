36d132719e82c76bb86eea8b0759c636bc114d73
AndersonDamN <a0@l4l.dk>
2015-05-07 14:31:37 +0200

    Added more comments.

--- src/gamePackage/MapBlock.java
+++ src/gamePackage/MapBlock.java
@@ -3,6 +3,7 @@ package gamePackage;
 import java.util.Random;
 
 public class MapBlock {
+	// Kernel size
 	int[][] size = new int[][]{
 			/*left*/	{1,1,1}, /*Top*/
 						{1,0,1},
@@ -17,12 +18,15 @@ public class MapBlock {
 	static int distFromCenterToCenter = 2;
 	
 	public MapBlock(){
+		// If the block has no exits
 		if(topLeft == false && topRight == false && bottomLeft == false && bottomRight == false){
+			// Give it 4 exits
 			topLeft = true; 
 			topRight = true; 
 			bottomLeft = true; 
 			bottomRight = true; 
 		}	
+		// Open the tiles with openings: 
 		if(topLeft == true){
 			size[0][1] = 0; 
 		}
@@ -44,10 +48,12 @@ public class MapBlock {
 		block.xCenter = x; 
 		block.yCenter = y;
 		
+		// The checklist is to make sure that two blocks cannot have the same position. 
 		GameLevel.checklist[x][y] = 1; 
 			for(int ki = -1; ki <= 1; ki++)
 				for(int kj = -1; kj <= 1; kj++)
-						GameLevel.gridTest[x+ki][y+kj] = block.size[ki+1][kj+1];
+					// Apply the block to its location in the map
+					GameLevel.gridTest[x+ki][y+kj] = block.size[ki+1][kj+1];
 					
 		// Check if there is an opening in topLeft && is the new block outside the map && is there already a block there?
 		if (block.topLeft == true && block.xCenter-distFromCenterToCenter > 0 && GameLevel.checklist[x-distFromCenterToCenter][y] == 0) {
@@ -70,6 +76,7 @@ public class MapBlock {
 			GameLevel.gridTest[x+1][y] = 0;
 		}
 		
+		// Makes sure that the new block will have an opening towards the block that came before
 		switch (priorBlock){
 		case 0: 
 			bottomRight = true; 
